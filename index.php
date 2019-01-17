04e94664da79b844dc62eb150ff0d200656dd7bc
Peter Anderson <peter.anderson@design-portfolio.co.uk>
2019-01-17 14:18:17 +0000

    0.01 Base commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Casa Alfarroba</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Google Web Font Embed -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/css/bootstrap.css" rel='stylesheet' type='text/css'>
+
+    <!-- Styles for this template -->
+    <link href="./assets/css/style.css" rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<div class="top-menu">
+    <div class="container">
+        <!-- Static navbar -->
+        <div class="navbar" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand"><img src="./assets/images/logo-white.png" alt="Casa Alfarroba" title="Casa Alfarroba" /></a>
+                </div>
+                <div class="navbar-collapse collapse" id="nav-bar">
+                    <ul class="nav navbar-nav navbar-right" style="margin-top: 40px;">
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#ameneties">Ameneties</a></li>
+                        <li><a href="#gallery">Gallery</a></li>
+                        <li><a href="#leisure">Leisure</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div><!--/.container-fluid -->
+        </div><!--/.navbar -->
+    </div> <!-- /container -->
+</div>
+
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js"  type="text/javascript"></script>
+<script src="js/stickUp.min.js"  type="text/javascript"></script>
+<script src="js/colorbox/jquery.colorbox-min.js"  type="text/javascript"></script>
+
+</body>
+</html>
