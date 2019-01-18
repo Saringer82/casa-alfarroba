b8aee8dd31b8f0bb9d0628b97b55305b6df9b4f8
Peter Anderson <peter.anderson@design-portfolio.co.uk>
2019-01-18 14:22:20 +0000

    0.1 Format

--- index.php
+++ index.php
@@ -7,13 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Google Web Font Embed -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic'
+          rel='stylesheet' type='text/css'>
 
     <!-- Bootstrap core CSS -->
     <link href="./assets/css/bootstrap.css" rel='stylesheet' type='text/css'>
 
     <!-- Styles for this template -->
     <link href="./assets/css/style.css" rel='stylesheet' type='text/css'>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 </head>
 
@@ -23,7 +27,6 @@
     <div class="container">
         <!-- Fixed navbar -->
         <div class="navbar" role="navigation">
-
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
@@ -31,7 +34,8 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a href="#home" class="navbar-brand"><img src="assets/images/logo-white.png" alt="Casa Alfarroba" title="Casa Alfarroba" /></a>
+                <a href="#home" class="navbar-brand"><img src="assets/images/logo-white.png" alt="Casa Alfarroba"
+                                                          title="Casa Alfarroba"/></a>
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
@@ -42,9 +46,8 @@
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
-
-        </div><!--/.navbar -->
-    </div> <!-- /container -->
+        </div>
+    </div>
 </div>
 
 <header>
@@ -54,33 +57,30 @@
     </div>
 </header>
 
-<?php include ("./sections/about.inc"); ?>
+<?php include("./sections/about.inc"); ?>
 
-<?php include ("./sections/amenities.inc"); ?>
+<?php include("./sections/amenities.inc"); ?>
 
-<?php include ("./sections/gallery.inc"); ?>
+<?php include("./sections/gallery.inc"); ?>
 
-<?php include ("./sections/leisure.inc"); ?>
+<?php include("./sections/leisure.inc"); ?>
 
-<?php include ("./sections/contact.inc"); ?>
+<?php include("./sections/contact.inc"); ?>
 
-<div class="footer" >
+<div class="footer">
     <div class="container">
         <div class="row">
-            <div class="text-center">
-                <div class="footer_bottom_content">
-                    <span id="footer-line">Copyright © <?php echo date("Y");?> <a href="#home">Casa Alfarroba</a></span>
-                </div>
-
-            </div>
+            <span>Copyright © <?php echo date("Y"); ?> <a
+                        href="#home">Casa Alfarroba</a></span>
+            <span><a href="#home"><i class="fas fa-arrow-circle-up"></i></a></span>
         </div>
     </div>
 </div>
 
 <script src="js/jquery.min.js" type="text/javascript"></script>
-<script src="js/bootstrap.min.js"  type="text/javascript"></script>
-<script src="js/stickUp.min.js"  type="text/javascript"></script>
-<script src="js/colorbox/jquery.colorbox-min.js"  type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/stickUp.min.js" type="text/javascript"></script>
+<script src="js/colorbox/jquery.colorbox-min.js" type="text/javascript"></script>
 
 </body>
 </html>
